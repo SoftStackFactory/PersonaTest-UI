e211cb896f965dde6ef1e1be40cb924d8a367685
SalMac86 <salvatore.mccarty@gmail.com>
2017-07-27 22:49:11 +0000

    getting lobby layout started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,16 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
+// import { HomePage } from '../pages/home/home';
 import { LobbyPage } from '../pages/lobby/lobby';
-import { RegisterPage } from '../pages/register/register';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
-
+  // rootPage:any = HomePage;
+  rootPage:any = LobbyPage;
+  
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
