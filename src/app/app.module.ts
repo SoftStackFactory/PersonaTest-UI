240330d74539974e4d39ac94db20e314855b0392
Peter Horton <phorton@softstackfactory.org>
2017-07-27 07:04:23 +0000

    added pages to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,22 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
+import { LandingPage } from '../pages/landing/landing';
 import { HomePage } from '../pages/home/home';
-import {  RegisterPage } from '../pages/register/register';
-import {  LobbyPage } from '../pages/lobby/lobby';
+import { RegisterPage } from '../pages/register/register';
+import { LobbyPage } from '../pages/lobby/lobby';
+import { TestListsPage } from '../pages/test-lists/test-lists';
+import { ResultsPage } from '../pages/results/results';
 
 const components = [ 
     MyApp,
+    LandingPage
     HomePage,
+    RegisterPage,
     LobbyPage,
-    RegisterPage
-    ]
+    TestListsPage,
+    ResultsPage
+  ]
 
 @NgModule({
   declarations: components,
