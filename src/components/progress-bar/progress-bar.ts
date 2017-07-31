b793684214a632121f6e412218a8caaf45209423
Jongmin Kim <kjm141215@gmail.com>
2017-07-30 22:13:22 -0700

    Add ProgressBarComponent to QuestionPage & Remove some unnecessary comments

--- src/components/progress-bar/progress-bar.ts
+++ src/components/progress-bar/progress-bar.ts
@@ -1,16 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
-/**
- * Generated class for the ProgressBarComponent component.
- *
- * See https://angular.io/docs/ts/latest/api/core/index/ComponentMetadata-class.html
- * for more info on Angular Components.
- */
 @Component({
   selector: 'progress-bar',
   templateUrl: 'progress-bar.html'
 })
 export class ProgressBarComponent {
+  @Input() totalQuestionNum: number;
+  @Input() questionNum: number;
   displayProgress(totalNum: number, currentNum: number) {
     let styles = {
       'background-color': '#ddd',
