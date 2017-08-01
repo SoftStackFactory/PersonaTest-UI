6896d504cdc6a7063a9ed63acfd6cbe6c98eb89a
Peter Horton <phorton@softstackfactory.org>
2017-08-01 01:48:29 +0000

    Generated test-list page and testlist component

--- src/pages/test-list/test-list.ts
+++ src/pages/test-list/test-list.ts
@@ -2,23 +2,23 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
 /**
- * Generated class for the TestListsPage page.
+ * Generated class for the TestListPage page.
  *
  * See http://ionicframework.com/docs/components/#navigation for more info
  * on Ionic pages and navigation.
  */
 
 @Component({
-  selector: 'page-test-lists',
-  templateUrl: 'test-lists.html',
+  selector: 'page-test-list',
+  templateUrl: 'test-list.html',
 })
-export class TestListsPage {
+export class TestListPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad TestListsPage');
+    console.log('ionViewDidLoad TestListPage');
   }
 
 }
