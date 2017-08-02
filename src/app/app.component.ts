144a470b473b57625f3ef0804496e062c9934bf4
Peter Horton <phorton@softstackfactory.org>
2017-08-02 19:50:32 +0000

    Peter check up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 //Pages
-import { QuicklinksPage } from '../pages/quicklinks/quicklinks';
+
 import { HomePage } from '../pages/home/home';
 import { LobbyPage } from '../pages/lobby/lobby';
 import { LoginPage } from '../pages/login/login';
@@ -12,8 +12,7 @@ import { RegisterPage } from '../pages/register/register';
 import { QuestionPage } from '../pages/question/question';
 import { FlexDemoPage } from '../pages/flex-demo/flex-demo';
 import { LandingPage } from '../pages/landing/landing';
-import { ResultsPage } from '../pages/results/results';
-import { HistoryPage } from '../pages/history/history';
+
 
 
 
@@ -22,7 +21,7 @@ import { HistoryPage } from '../pages/history/history';
 })
 export class MyApp {
 
-  rootPage:any = QuicklinksPage;
+  rootPage:any = LobbyPage 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
