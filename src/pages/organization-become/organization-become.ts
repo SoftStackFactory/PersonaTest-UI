2eb1b165809b2c030087aa2ae322a543f40c71c4
Peter Horton <marenkb@gmail.com>
2017-08-03 23:02:37 +0000

    navigation to lobby page

--- src/pages/organization-become/organization-become.ts
+++ src/pages/organization-become/organization-become.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
-<<<<<<< HEAD
 import { LobbyPage } from '../lobby/lobby';
 
-=======
->>>>>>> basic content for page to become and organization
 /**
  * Generated class for the OrganizationBecomePage page.
  *
@@ -18,42 +15,18 @@ import { LobbyPage } from '../lobby/lobby';
   templateUrl: 'organization-become.html',
 })
 export class OrganizationBecomePage {
-<<<<<<< HEAD
-  organization: any = {}
-  userEmail: string
-  constructor(
-    public navCtrl: NavController, 
-    public navParams: NavParams
-  ) {}
-=======
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, 
+  public navParams: NavParams) {
   }
->>>>>>> basic content for page to become and organization
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad OrganizationBecomePage');
   }
-<<<<<<< HEAD
   showEula() {
     alert("Terms and Conditions Placeholder");
   }
-  requestOrg(form) {
-    let email = {
-      to: 'administrator',
-      subject: "New Organization Request, " + this.organization.name,
-      body: "Organization Name: " + this.organization.name + 
-        "/n Organization Webpage: " + this.organization.webPage +
-        "/n Submitted by: " + this.userEmail,
-      isHtml: true
-    }
-   // this.emailComposer.open(email);
+  requestOrg() {
     this.navCtrl.push(LobbyPage);
   }
-  goHome() {
-    this.navCtrl.push(LobbyPage);
-  }
-=======
-
->>>>>>> basic content for page to become and organization
 }
--- src/pages/organization-manage/organization-manage.ts
+++ src/pages/organization-manage/organization-manage.ts
@@ -1,15 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-<<<<<<< HEAD
-import { ToastController } from 'ionic-angular';
-import { ModalController } from 'ionic-angular';
-
-import { TestSelectionComponent } from '../../components/test-selection/test-selection';
+import { EmailComposer } from '@ionic-native/email-composer';
 
 import { LobbyPage } from '../lobby/lobby';
-=======
-import { EmailComposer } from '@ionic-native/email-composer';
->>>>>>> styling and some content for the manage organization page
 
 /**
  * Generated class for the OrganizationManagePage page.
@@ -23,23 +16,6 @@ import { EmailComposer } from '@ionic-native/email-composer';
   templateUrl: 'organization-manage.html',
 })
 export class OrganizationManagePage {
-<<<<<<< HEAD
-  
-  //Variables to store organization and user information
-  organization: any = {}
-  organizationName: string
-  organizationLogo: any
-  userEmail: string
-  
-  constructor(public navCtrl: NavController, 
-    public navParams: NavParams,
-    public toastCtrl: ToastController,
-    public modalCtrl: ModalController) {
-      this.organizationName = "SoftStack Factory";
-      this.organizationLogo = "placeholder";
-      this.userEmail = "Peter@SoftStack.org";
-    }
-=======
   organizationName: string
   organizationLogo: any
   userEmail: string
@@ -50,7 +26,6 @@ export class OrganizationManagePage {
       this.organizationLogo = "placeholder";
       this.userEmail = "Peter@SoftStack.org";
   }
->>>>>>> styling and some content for the manage organization page
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad OrganizationManagePage');
@@ -85,6 +60,8 @@ export class OrganizationManagePage {
     this.navCtrl.push(LobbyPage);
 =======
   submitChanges(form) {
+    /*
+    //generate email requesting to administrator
     if(form.invalid) {
       return alert("Please make changes before submitting.");
     }
@@ -98,7 +75,88 @@ export class OrganizationManagePage {
       '/n ',
       isHtml: true
     };
->>>>>>> styling and some content for the manage organization page
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import { EmailComposer } from '@ionic-native/email-composer';
+
+import { LobbyPage } from '../lobby/lobby';
+
+/**
+ * Generated class for the OrganizationManagePage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+
+@Component({
+  selector: 'page-organization-manage',
+  templateUrl: 'organization-manage.html',
+})
+export class OrganizationManagePage {
+  organizationName: string
+  organizationLogo: any
+  userEmail: string
+  constructor(public navCtrl: NavController, 
+    public navParams: NavParams,
+    private emailComposer: EmailComposer) {
+      this.organizationName = "SoftStack Factory";
+      this.organizationLogo = "placeholder";
+      this.userEmail = "Peter@SoftStack.org";
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad OrganizationManagePage');
+  }
+<<<<<<< HEAD
+  
+  submitChanges(form) {
+    //generate email requesting to administrator
+    if(form.invalid) {
+      console.log("No Change Email to send.")
+      return alert("Please make changes before submitting.");
+    } else {
+      let email = {
+        to: 'administrator',
+        attachments: [
+          'file://img/logo.png',
+        ],
+        subject: this.organizationName + ' Change Request',
+        body: 'changes requested by ' + this.userEmail +
+        '/n ',
+        isHtml: true
+      };
+      console.log("Change Email Sent");
+      this.navCtrl.push(LobbyPage);
+      //return this.emailComposer.open(email);
+    }
+  }
+  
+
+  //home button at top right
+  goHome() {
+    this.navCtrl.push(LobbyPage);
+=======
+  submitChanges(form) {
+    /*
+    //generate email requesting to administrator
+    if(form.invalid) {
+      return alert("Please make changes before submitting.");
+    }
+    let email = {
+      to: 'administrator',
+      attachments: [
+        'file://img/logo.png',
+      ],
+      subject: this.organizationLogo + ' Change Request',
+      body: 'changes requested by ' + this.userEmail +
+      '/n ',
+      isHtml: true
+    };
+    */
+    this.navCtrl.push(LobbyPage);
+  }
+  goHome() {
+    this.navCtrl.push(LobbyPage);
   }
   
 }
