72b468ff4c0e92f106f6ee48b0e07490eb139ce7
SalMac86 <salvatore.mccarty@gmail.com>
2017-08-03 02:48:39 +0000

    starting out IonicView feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,7 +21,7 @@ import { HistoryPage } from '../pages/history/history';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = QuicklinksPage;
+  rootPage:any = LobbyPage;
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
