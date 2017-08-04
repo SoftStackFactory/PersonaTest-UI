80610710c4f42b951bd3cc7c3f64c50bdfff7df5
vincenthoang00@gmail.com <jpbrown@softstackfactory.org>
2017-08-04 22:04:33 +0000

    copy and pasted original code over the merge conflicted code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -43,7 +43,7 @@ let injections: any[] = [
     ChartComponent,
     TestlistComponent,
     RegistrationComponent,
-    ProgressBarComponent
+    ProgressBarComponent,
     SearchHistoryComponent
 
     ]
