d447e30504844d0dae8068c3fda0454c37b6756d
vincenthoang00@gmail.com <jpbrown@softstackfactory.org>
2017-08-05 03:50:31 +0000

    user info successfully gets stored in object

--- src/components/registration/registration.ts
+++ src/components/registration/registration.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+import { LobbyPage } from '../../pages/lobby/lobby';
 
 /**
  * Generated class for the RegistrationComponent component.
@@ -11,12 +14,36 @@ import { Component } from '@angular/core';
   templateUrl: 'registration.html'
 })
 export class RegistrationComponent {
+  
+  user: any = {}
+  eula: boolean;
+  
+  checkEula() {
+    console.log("Terms and Conditions have been accepted", this.eula);
+  }
+  
+  signupForm(form) {
+    if(form.invalid) {
+      return alert("Please fill in all fields.");
+    } if(this.user.password !== this.user.confirmPassword) {
+      //Passwords did not match, delete user passwords
+      this.user.password = null;
+      this.user.confirmPassword = null;
+      return alert("Your passwords did not match. Please re-enter your passwords");
+    } if(this.eula == false) {
+      //User must agree to terms and conditions before registering
+      return alert("Please check the box to agree to our Terms and Conditions");
+    }
+    delete this.user.confirmPassword;
+    console.log(this.user);
+    // Navigate to lobby
+    this.navCtrl.setRoot(LobbyPage);
+  }
 
-  text: string;
-
-  constructor() {
-    console.log('Hello RegistrationComponent Component');
-    this.text = 'Hello World';
+  constructor(
+    private navCtrl : NavController
+    ) {
+    console.log('Hello Registration Component');
   }
 
 }
