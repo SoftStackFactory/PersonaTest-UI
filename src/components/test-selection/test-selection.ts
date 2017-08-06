e62b37de3234d6991988c79b9769949342ee9156
Peter Horton <marenkb@gmail.com>
2017-08-06 20:47:01 +0000

    separation of change test element

--- src/components/test-selection/test-selection.ts
+++ src/components/test-selection/test-selection.ts
@@ -12,29 +12,11 @@ import { Component } from '@angular/core';
 })
 export class TestSelectionComponent {
 
-  //Variable to store array of test as an array of objects and mock data (thanks Jenny)
-  availableTests: { name: string, description: string }[] =
-  [{name: "Goldberg's Big Five", description: "some text"}, 
-  {name:"Markey and Markey's", description: "some text"},
-  {name: "Costa and McCrae's NEO Facets", description: "some text"},
-  {name:"Johnson's 120 Item NEO", description: "some text"}];
-  testDescriptions: any
-  
+  text: string;
 
   constructor() {
-    console.log('Hello Test Selection Component');
-    /*this.availableTests = 
-      [{name: "Goldberg's Big Five", description: "some text"}, 
-      {name:"Markey and Markey's", description: "some text"},
-      {name: "Costa and McCrae's NEO Facets", description: "some text"},
-      {name:"Johnson's 120 Item NEO", description: "some text"}];
-    function(){
-        for (i = 0,  len = this.availableTests.length; i < len; i++) {
-          return this.testDescriptions += 
-            this.availableTests[i][name] + ": " + 
-            this.availableTests[i][description] + "/n";
-        }
-        */
+    console.log('Hello TestSelectionComponent Component');
+    this.text = 'Hello World';
   }
 
 }
