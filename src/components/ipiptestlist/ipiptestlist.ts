3416813689789fe9625378a67fb6270bebaa4b5a
Dillon Dobson <dmartinezdobson@gmail.com>
2017-08-08 00:20:06 +0000

    IPIP Testlist 1.0 with limited css styling to set cards at max width and attempt to center cards when in desktop view

--- src/components/ipiptestlist/ipiptestlist.ts
+++ src/components/ipiptestlist/ipiptestlist.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { QuestionPage } from '../../pages/question/question'; 
+import { NavController, NavParams } from 'ionic-angular';
+
+// import { Platform } from 'ionic-angular';
+
 
 /**
  * Generated class for the IpiptestlistComponent component.
@@ -9,14 +14,51 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ipiptestlist',
   templateUrl: 'ipiptestlist.html'
+//     template: `
+//     <ion-header>
+//       <ion-navbar>
+//         <ion-title>Tabs</ion-title>
+//       </ion-navbar>
+//     </ion-header>
+//     <ion-content>
+//     </ion-content>
+// `
 })
+  
 export class IpiptestlistComponent {
 
   text: string;
-
-  constructor() {
-    console.log('Hello IpiptestlistComponent Component');
-    this.text = 'Hello World';
+  // isAndroid: boolean = false;
+    
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    ) {
+      console.log('Hello IpiptestlistComponent Component');
+      this.text = 'Hello';
+      
+  // constructor(platform: Platform) {
+  //   console.log('Hello IpiptestlistComponent Component');
+  //   this.text = 'Hello';
+  //   this.isAndroid = platform.is('android');
+  
+  }
+    toQuestionPage() {
+    console.log('to question page');
+    this.navCtrl.push(QuestionPage);
   }
 
 }
+
+// @Component({
+//   template: `
+//     <ion-tabs class="tabs-basic">
+//       <ion-tab tabTitle="Music" [root]="rootPage"></ion-tab>
+//       <ion-tab tabTitle="Movies" [root]="rootPage"></ion-tab>
+//       <ion-tab tabTitle="Games" [root]="rootPage"></ion-tab>
+//     </ion-tabs>
+// `})
+
+// export class BasicPage {
+//   rootPage = IpiptestlistComponent;
+// }
