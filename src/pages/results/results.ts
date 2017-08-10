13577f28224c465a8ead2fa5cf9b02307f797262
jbrownssf <gerardosoto93@gmail.com>
2017-08-10 00:52:47 +0000

    Added logout component to results page

--- src/pages/results/results.ts
+++ src/pages/results/results.ts
@@ -3,6 +3,7 @@ import { NavController, NavParams } from 'ionic-angular';
 
 import { ChartComponent } from '../../components/chart/chart';
 import { TestlistComponent } from '../../components/testlist/testlist';
+import { LogoutComponent } from '../../components/logout/logout';
 
 
 
