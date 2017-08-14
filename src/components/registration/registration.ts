cbb7aee9fa9cf8ab8669aac74bee72e8edd0120b
vhoangssf <vincenthoang00@gmail.com>
2017-08-14 20:57:13 +0000

    added server side errors and ionic style alerts

--- src/components/registration/registration.ts
+++ src/components/registration/registration.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { NavController, ModalController } from 'ionic-angular';
+import { NavController, ModalController, AlertController } from 'ionic-angular';
 
 import { LobbyPage } from '../../pages/lobby/lobby';
 import { LoginPage } from '../../pages/login/login';
 import { EulaComponent } from '../eula/eula';
+import { AppUser } from '../../providers/app-user';
 
 
 /**
@@ -19,10 +20,14 @@ import { EulaComponent } from '../eula/eula';
 export class RegistrationComponent {
   user: any = {}
   eula: boolean
+  alertTitle: string
+  alertSubtitle: string
   
   constructor(
     private navCtrl: NavController,
-    private modalCtrl: ModalController
+    private modalCtrl: ModalController,
+    private alertCtrl: AlertController,
+    private appUser: AppUser
     ) {
     console.log('Hello Registration Component');
   }
@@ -31,30 +36,67 @@ export class RegistrationComponent {
     console.log("User has agreed to Terms & Conditions", this.eula);
   }
   
+  showAlert() {
+    let alert = this.alertCtrl.create({
+      title: this.alertTitle,
+      subTitle: this.alertSubtitle,
+      buttons: ["Dismiss"]
+    });
+    alert.present();
+  }
+  
   signupForm(form) {
     if(form.invalid) {
-      return alert("Please fill in all fields.");
+      this.alertTitle = "Invalid Form";
+      this.alertSubtitle = "Please fill in all required fields.";
+      return this.showAlert();
       
       //Passwords did not match, delete user passwords
-    } if(this.user.password !== this.user.confirmPassword) {
+    } else if(this.user.password !== this.user.confirmPassword) {
       this.user.password = null;
       this.user.confirmPassword = null;
-      return alert("Your passwords did not match. Please re-enter your passwords");
+      this.alertTitle = "Passwords do not match";
+      this.alertSubtitle = "Please re-enter your passwords.";
+      return this.showAlert();
       
        //User must agree to terms and conditions before registering
-    } if(this.eula !== true) {
-      return alert("Please check the box to agree to our Terms and Conditions");
+    } else if(this.user.isEula !== true) {
+      this.alertTitle = "Terms & Conditions";
+      this.alertSubtitle = "Please check the box to accept our Terms & Conditions.";
+      return this.showAlert();
     }
     
     //successfull registration
     delete this.user.confirmPassword;
     console.log(this.user);
-    
-    // Navigate to lobby
-    this.navCtrl.setRoot(LobbyPage);
+    this.appUser.register(this.user)
+      .map(res => res.json())
+      .subscribe(res => {
+        window.localStorage.setItem('token', res.token);
+        window.localStorage.setItem('id', res.id)
+        this.navCtrl.setRoot(LobbyPage);
+        
+      }, error => {
+        //Server side errors
+        if (error.status === 404) {
+          this.alertTitle = "404";
+          this.alertSubtitle = "Not Found.";
+          return this.showAlert();
+          
+        } else if (error.status === 422) {
+          this.alertTitle = "422";
+          this.alertSubtitle = "Invalid email address or email is already taken";
+          return this.showAlert();
+          
+        } else if (error.status === 500) {
+          this.alertTitle = "500";
+          this.alertSubtitle = "Server is currently offline, please try again in a few minutes.";
+          return this.showAlert();
+     }  
+      });
   }
   
-  //When user clicks 'Terms and Conditions', display EULA through modal
+  //When user clicks 'View Terms and Conditions', display EULA through modal
   showEula() {
     let modal = this.modalCtrl.create(EulaComponent);
     modal.present();
