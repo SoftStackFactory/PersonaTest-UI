b5948f7a6ab454eef2038b7f672092b4316962d7
jbrownssf <gerardosoto93@gmail.com>
2017-08-22 02:05:06 +0000

    Created a mock answers json, called json file via http, created reduce function to tally up answer selections

--- src/pages/results/results.ts
+++ src/pages/results/results.ts
@@ -1,33 +1,60 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+// Providers
+import { ResultsProvider } from '../../providers/results/results';
+
+// Components
 import { ChartComponent } from '../../components/chart/chart';
 import { TestlistComponent } from '../../components/testlist/testlist';
 import { LogoutComponent } from '../../components/logout/logout';
 
-
-
-/**
- * Generated class for the ResultsPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 
 @Component({
   selector: 'page-results',
   templateUrl: 'results.html',
 })
 export class ResultsPage {
-
   
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  
+  private answers: any = {}; 
+  
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    private resultsProvider: ResultsProvider
+    ) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ResultsPage');
+    this.resultsProvider.getAnswers().subscribe(
+      answers => {
+        this.answers = answers;
+        console.log("answers are here", answers);
+      }, error => {
+        alert("Answers not successfully provided!");
+        console.log(error);
+      },() =>{ 
+      this.gradeTest(this.answers);
+        
+      }
+    )
   }
   
-
+ 
+  
+  gradeTest(test) {
+    let results = test.reduce(function(total, value) {
+      let category = value.category;
+      if(category in total) {
+        total[category] += value.selection
+      } else {
+        total[category] = value.selection
+      } return total
+    }, {})
+    console.log("grades", results);
+  }
+ 
 
 }
--- src/providers/results/results.ts
+++ src/providers/results/results.ts
@@ -1,14 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
-import { URL } from '../../assets/provider-config'
+import { URL } from '../../assets/provider-config';
 
-/*
-  Generated class for the ResultsProvider provider.
 
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular DI.
-*/
+
+
 @Injectable()
 export class ResultsProvider {
   
@@ -20,30 +17,33 @@ export class ResultsProvider {
     console.log('Hello ResultsProvider Provider');
   }
   
+  getAnswers() {
+    return this.http.get('../../assets/mockSavedAnswers.json').map(res => res.json());
+  }
+
   
-  //Retrieve test results with access token
-  getTestResults(token, userId){
-    return this.http.get(this.baseUrl + this.path + 
-      `?filter[where][userId]=${ userId }` +
-      `&access_token=${ token }`
-    );
-  }
+  // //Retrieve test results with access token
+  // getTestResults(token, userId){
+  //   return this.http.get(this.baseUrl + this.path + 
+  //     `?filter[where][userId]=${ userId }` +
+  //     `&access_token=${ token }`
+  //   );
+  // }
 
-  //Save a new test result to the backend
-  saveTest(token, testResult){
-    return this.http.post(this.baseUrl + this.path + 
-      '?access_token=' + token, 
-      testResult
-    );
-  }
+  // //Save a new test result to the backend
+  // saveTest(token, testResult){
+  //   return this.http.post(this.baseUrl + this.path + 
+  //     '?access_token=' + token, 
+  //     testResult
+  //   );
+  // }
 
-  //Delete a test result according to id
-  deleteTest(testId){
-    return this.http.delete(this.baseUrl + this.path + 
-      '/' + 
-      testId
-    );
-  }
-
+  // //Delete a test result according to id
+  // deleteTest(testId){
+  //   return this.http.delete(this.baseUrl + this.path + 
+  //     '/' + 
+  //     testId
+  //   );
+  // }
 
 }
