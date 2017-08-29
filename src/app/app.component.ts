27ae25ed34ca17e29d9eecf3717632e154b6ffcf
christopherwk <cwkhem1@gmail.com>
2017-08-29 01:48:50 +0000

    fixed a little on landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -38,7 +38,7 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
-      this.translate.setDefaultLang('sp');
+      this.translate.setDefaultLang('en');
     });
   }
 }
