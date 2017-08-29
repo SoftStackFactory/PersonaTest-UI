01733d9ae5579e46a1599a844053c9716fc67acc
Gerardo Soto <gerardosoto93@gmail.com>
2017-08-28 19:18:07 -0700

    Feature/display answers (#95)
    * Commented code + initialized test object
    
    * Initialized test object when clicking to take a test
    
    * passed testTaken object via NavParams to the questions page
    
    * Filtered tests by each test instance
    
    * Changed Chart type from polar to radar

--- src/components/chart/chart.ts
+++ src/components/chart/chart.ts
@@ -12,94 +12,106 @@ export class ChartComponent implements AfterContentInit {
   polarChart: any;
 
   ngAfterContentInit() {
-    console.log("chart", this.gradedTest);
-    this.polarChart = new Chart(this.polarCanvas.nativeElement, {
-      type: 'polarArea',
-      data: {
-        labels: [
-          "Agreeableness",
-          "Conscientiousness",
-          "Emotional Stability",
-          "Extraversion",
-          "Intellect"
-          ],
-        datasets: [{
-          data: [
-            this.gradedTest["Agreeableness"], 
-            this.gradedTest["Conscientiousness"], 
-            this.gradedTest["Emotional Stability"], 
-            this.gradedTest["Extraversion"], 
-            this.gradedTest["Intellect"]
-            ],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)'
-          ],
-          borderColor: [
-            'rgba(255,99,132,1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)'
-          ]
-        }]
-      },
-      options: {
-        responsive: true,
-        animation: {
-          animationScale: true
-        },
-        scale: {
-          ticks: {
-            beginAtZero: true,
-            stepSize: 5,
-          }
-        }
-      }
-    });
-    
-    //Radar Chart can be used to compare multiple tests/users
-    
+    // console.log("chart", this.gradedTest);
     // this.polarChart = new Chart(this.polarCanvas.nativeElement, {
-    //   type: 'radar',
-    // data: {
-    //   labels: [
+    //   type: 'polarArea',
+    //   data: {
+    //     labels: [
     //       "Agreeableness",
     //       "Conscientiousness",
     //       "Emotional Stability",
     //       "Extraversion",
     //       "Intellect"
     //       ],
-    //   datasets: [
-    //     {
-    //       label: "Test #1",
-    //       fill: true,
-    //       backgroundColor: 'rgba(255, 99, 132, 0.2)',
-    //       borderColor: 'rgba(255,99,132,1)',
-    //       pointBorderColor: "#fff",
-    //       pointBackgroundColor: 'rgba(255, 99, 132, 0.2)',
-    //       data: [40, 50, 20, 30, 10]
-    //     }, {
-    //       label: "Test #2",
-    //       fill: true,
-    //       backgroundColor: 'rgba(54, 162, 235, 0.2)',
-    //       borderColor: 'rgba(54, 162, 235, 1)',
-    //       pointBorderColor: "#fff",
-    //       pointBackgroundColor: 'rgba(54, 162, 235, 0.2)',
-    //       data: [20, 30, 50, 10, 40]
-    //     }
-    //   ]
-    // },
+    //     datasets: [{
+    //       data: [
+    //         this.gradedTest["Agreeableness"], 
+    //         this.gradedTest["Conscientiousness"], 
+    //         this.gradedTest["Emotional Stability"], 
+    //         this.gradedTest["Extraversion"], 
+    //         this.gradedTest["Intellect"]
+    //         ],
+    //       backgroundColor: [
+    //         'rgba(255, 99, 132, 0.2)',
+    //         'rgba(54, 162, 235, 0.2)',
+    //         'rgba(255, 206, 86, 0.2)',
+    //         'rgba(75, 192, 192, 0.2)',
+    //         'rgba(153, 102, 255, 0.2)'
+    //       ],
+    //       borderColor: [
+    //         'rgba(255,99,132,1)',
+    //         'rgba(54, 162, 235, 1)',
+    //         'rgba(255, 206, 86, 1)',
+    //         'rgba(75, 192, 192, 1)',
+    //         'rgba(153, 102, 255, 1)'
+    //       ]
+    //     }]
+    //   },
     //   options: {
-    //     title: {
-    //       display: true,
-    //       text: 'IPIP Test Results'
+    //     responsive: true,
+    //     animation: {
+    //       animationScale: true
+    //     },
+    //     scale: {
+    //       ticks: {
+    //         beginAtZero: true,
+    //         stepSize: 5,
+    //       }
     //     }
     //   }
     // });
+    
+    // Radar Chart can be used to compare multiple tests/users
+    
+    this.polarChart = new Chart(this.polarCanvas.nativeElement, {
+      type: 'radar',
+    data: {
+      labels: [
+          "Agreeableness",
+          "Conscientiousness",
+          "Emotional Stability",
+          "Extraversion",
+          "Intellect"
+          ],
+      datasets: [
+        {
+          label: "Test #1",
+          fill: true,
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          borderColor: 'rgba(255,99,132,1)',
+          pointBorderColor: "#fff",
+          pointBackgroundColor: 'rgba(255, 99, 132, 0.2)',
+          data: [
+            this.gradedTest["Agreeableness"], 
+            this.gradedTest["Conscientiousness"], 
+            this.gradedTest["Emotional Stability"], 
+            this.gradedTest["Extraversion"], 
+            this.gradedTest["Intellect"]
+            ]
+        }, {
+          label: "Test #2",
+          fill: true,
+          backgroundColor: 'rgba(54, 162, 235, 0.2)',
+          borderColor: 'rgba(54, 162, 235, 1)',
+          pointBorderColor: "#fff",
+          pointBackgroundColor: 'rgba(54, 162, 235, 0.2)',
+          data: [
+            this.gradedTest["Agreeableness"], 
+            this.gradedTest["Conscientiousness"], 
+            this.gradedTest["Emotional Stability"], 
+            this.gradedTest["Extraversion"], 
+            this.gradedTest["Intellect"]
+            ]
+        }
+      ]
+    },
+      options: {
+        title: {
+          display: true,
+          text: 'IPIP Test Results'
+        }
+      }
+    });
     
   }
 
