026c3afd40cec1051a446a87003f73ac7915d672
vhoangssf <vincenthoang00@gmail.com>
2017-08-31 03:12:03 +0000

    registration page redone

--- src/components/registration/registration.ts
+++ src/components/registration/registration.ts
@@ -28,7 +28,6 @@ export class RegistrationComponent {
   alertSubtitle: string;
   registerForm: FormGroup;
   submitAttempt: boolean = false;
-  // eulaError: any;
   checked: boolean = false;
   
   constructor(
@@ -110,57 +109,6 @@ export class RegistrationComponent {
         }
     });
   }
-  
-  // signupForm(form) {
-  //   if(form.invalid) {
-  //     this.alertTitle = "Invalid Form";
-  //     this.alertSubtitle = "Please fill in all required fields.";
-  //     return this.showAlert();
-      
-  //     //Passwords did not match, delete user passwords
-  //   } else if(this.user.password !== this.user.confirmPassword) {
-  //     this.user.password = null;
-  //     this.user.confirmPassword = null;
-  //     this.alertTitle = "Passwords do not match";
-  //     this.alertSubtitle = "Please re-enter your passwords.";
-  //     return this.showAlert();
-      
-  //     //User must agree to terms and conditions before registering
-  //   } else if(this.user.isEula !== true) {
-  //     this.alertTitle = "Terms & Conditions";
-  //     this.alertSubtitle = "Please check the box to accept our Terms & Conditions.";
-  //     return this.showAlert();
-  //   }
-    
-  //   //successfull registration
-  //   delete this.user.confirmPassword;
-  //   console.log(this.user);
-  //   this.appUser.register(this.user)
-  //     .map(res => res.json())
-  //     .subscribe(res => {
-  //       window.localStorage.setItem('token', res.token);
-  //       window.localStorage.setItem('id', res.id)
-  //       this.navCtrl.setRoot(LobbyPage);
-        
-  //     }, error => {
-  //       //Server side errors
-  //       if (error.status === 404) {
-  //         this.alertTitle = "404";
-  //         this.alertSubtitle = "Not Found.";
-  //         return this.showAlert();
-          
-  //       } else if (error.status === 422) {
-  //         this.alertTitle = "422";
-  //         this.alertSubtitle = "Invalid email address or email is already taken";
-  //         return this.showAlert();
-          
-  //       } else if (error.status === 500) {
-  //         this.alertTitle = "500";
-  //         this.alertSubtitle = "Server is currently offline, please try again in a few minutes.";
-  //         return this.showAlert();
-  //       }    
-  //     });
-  // }
   
   //When user clicks 'View Terms and Conditions', display EULA through modal
   showEula(){
