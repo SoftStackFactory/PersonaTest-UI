430180e82d148e04bd2a609b0a60d3552c0f46b1
christopherwk <cwkhem1@gmail.com>
2017-09-03 05:03:28 +0000

    merge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -54,7 +54,7 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
         statusBar.styleDefault();
         splashScreen.hide();
-        this.translate.setDefaultLang('sp');
+        this.translate.setDefaultLang('en');
     });
   };
   
