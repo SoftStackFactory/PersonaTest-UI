d0682e64ddd40d22a2092cadbfef5646ac8e46e5
Peter Horton <marenkb@gmail.com>
2017-09-12 04:09:52 +0000

    added menu to results page, removed home icon

--- src/pages/results/results.ts
+++ src/pages/results/results.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, MenuController } from 'ionic-angular';
 
 // Providers
 import { AnswersProvider } from '../../providers/answers/answers';
@@ -25,6 +25,7 @@ export class ResultsPage {
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
+    public menuCtrl: MenuController,
     private answersProvider: AnswersProvider,
     private resultsProvider: ResultsProvider,
     ) {
