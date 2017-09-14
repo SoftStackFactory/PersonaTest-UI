70104ac7a2f78f2f597b07a9bb5a2a2058b9f6e2
J Tran <j3nnytran@yahoo.com>
2017-09-14 02:44:00 +0000

    Removed mock data in history page

--- src/pages/history/history.ts
+++ src/pages/history/history.ts
@@ -26,12 +26,7 @@ export class HistoryPage {
   emptyHistory: boolean = true;
   
   //Variable to store our array of test as an array of objects; Currently using mock data
-  ourList: { name: string, date: Date }[] = 
-  [{name: "Goldberg's Big Five", date: new Date('August 22, 2017')}, 
-  {name: "Goldberg's Big Five", date: new Date('August 20, 2016')}, 
-  {name:"Markey and Markey's", date: new Date('Oct 4, 2016') },
-  {name: "Costa and McCrae's NEO Facets", date: new Date('Jan 22, 2016')},
-  {name:"Johnson's 120 Item NEO", date: new Date('April 4, 2010')}];
+  ourList: { name: string, date: Date }[] = {};
   
   //Variable to store copy of our original array, because we will need to filter
   filteredList: any = this.ourList;
